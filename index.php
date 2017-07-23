4a675de9b0ac76617c9406fe36364df9047298cc
wangren <819699622@qq.com>
2017-07-23 18:51:46 +0800

    删除

--- index.php
+++ index.php
@@ -2,6 +2,3 @@
 echo "hello word!";
 echo "git";
 echo "459";
-echo "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
-echo "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
-echo "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
